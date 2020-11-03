4a8b86f326d50268f6de9f20a03437cf8a124851
rhackon <al375877@uji.es>
2020-11-03 12:21:30 +0100

    Finalizado

--- Entregable1/entregable1.py
+++ Entregable1/entregable1.py
@@ -4,47 +4,43 @@ from sys import *
 from algoritmia.datastructures.digraphs import UndirectedGraph
 from algoritmia.datastructures.mergefindsets import MergeFindSet
 from random import shuffle, seed
-# from labyrinthviewer import LabyrinthViewer
+from labyrinthviewer import LabyrinthViewer
 
 Vertex = Tuple[int, int]
 Edge = Tuple[Vertex, Vertex]
 
 def laberintoMensaje(fichEntrada) -> UndirectedGraph:
 
-    num_filas=fichEntrada[0][0]
-    num_cols=fichEntrada[0][1]
-    num_paredesProhib = fichEntrada[1][0]  #No se para que utilizarlo
-    listaProhibidas = []
-    for elem in fichEntrada[2:len(fichEntrada)]:
-        u = (elem[0], elem[1])
-        v = (elem[2], elem[3])
-        arista = (u,v)
-        listaProhibidas.append(arista)
-    #***********************************************
-    vertices: List[Vertex] = [(r,c) for r in range(num_filas) for c in range(num_cols)]
+    num_filas=int(fichEntrada[0][0])
+    num_cols=int(fichEntrada[0][1])
+    num_paredesProhib = int(fichEntrada[1][0])  #No se para que utilizarlo
+    conjuntoProhibidas = set(fichEntrada[2:len(fichEntrada)]) #Conjunto es más rapido 0(1)
 
     mfs: MergeFindSet[Vertex] = MergeFindSet()
-    for v in vertices:
-        mfs.add(v)
-
+    vertices: List[Vertex] = []
     edges: List[Edge] = []
-    # Creamos la arista de arriba e izquierda de cada vertice
-    for r, c in vertices:  # r,c = (r,c) = v Desglolsas la tupla
-        if r > 0:
-            edges.append(((r, c), (r - 1, c)))
-        if c > 0:
-            edges.append(((r, c), (r, c - 1)))
+    corridors: List[Edge] = []  # pasillos de nuestro grafo
+
+    #Hacemos msf, y la lista de aristas en el mismo bucle anidado, para quitarnos bucles y ganar rapidez
+    for r in range(num_filas):
+        for c in range(num_cols):
+            vertices.append((r,c))
+            mfs.add((r,c))
+            # Creamos la arista de arriba e izquierda de cada vertice
+            if r > 0:
+                edges.append(((r, c), (r - 1, c)))
+            if c > 0:
+                edges.append(((r, c), (r, c - 1)))
 
     shuffle(edges)  # baraja las aristas
 
-    corridors: List[Edge] = []  # pasillos de nuestro grafo
-    for elem2 in edges:
-        u2=elem2[0]
-        v2=elem2[1]
-        if (mfs.find(u2) != mfs.find(v2)):
-            if ((u2, v2) not in listaProhibidas and (v2, u2) not in listaProhibidas):
-                mfs.merge(u2, v2)
-                corridors.append((u2, v2))
+    for elem in edges:
+        u=elem[0]
+        v=elem[1]
+        if (mfs.find(u) != mfs.find(v)):
+            if ((v, u) not in conjuntoProhibidas): # Comprabamos que no es una pared prohibida
+                mfs.merge(u, v)
+                corridors.append((u, v))
 
     # Comprabación de que entra en todas las habitaciones
     valido=True
@@ -54,7 +50,6 @@ def laberintoMensaje(fichEntrada) -> UndirectedGraph:
     return UndirectedGraph(E=corridors), (num_filas,num_cols), len(corridors), corridors, valido
 
 #**************Comprobación si es conexo o no + cliclos*******************************************
-
 def recorredor_vertices_profundidad(grafo: UndirectedGraph, v_inicial: Vertex) -> List[Edge]:
     def recorrido_desde(v):
         seen.add(v)
@@ -76,7 +71,7 @@ def componentes_conexos(g: UndirectedGraph) -> "List[List[Vertex]]":
         vertices_visitados = recorredor_vertices_profundidad(g, u)
         vertices_no_visitados -= set(vertices_visitados)
         resultado.append(vertices_visitados)
-        if (len(resultado)>1): #Si no es conexo, salir
+        if (len(resultado)>1): #Si ya tiene más de un grafo, salimos.
             es_conexo=False
             break
     return es_conexo
@@ -85,15 +80,22 @@ def componentes_conexos(g: UndirectedGraph) -> "List[List[Vertex]]":
 def leerFichero(fichEntrada):
     fich = open(fichEntrada, "r", encoding="utf-8")
     lista = []
+    i=0
     for linea in fich:
-        elem = [int(x) for x in linea.rstrip("\n").split(" ")]
-        elem = tuple(elem)  # Convertimos lista a tupla
-        lista.append(elem)
+        i+=1
+        elem=linea.rstrip("\n").split(" ")
+        elem=tuple(elem)
+        if (i>=3): #Formato arista
+            u = (int(elem[0]), int(elem[1]))
+            v = (int(elem[2]), int(elem[3]))
+            lista.append((u,v))
+        else:
+            lista.append(elem)
     return lista  # Devolvemos lista de tuplas, cada elemento una linea del fichero
 
-# def visualizaLaberinto(graph):
-#     lv = LabyrinthViewer(graph, canvas_width=900, canvas_height=900, margin=10)
-#     lv.run()
+def visualizaLaberinto(graph):
+    lv = LabyrinthViewer(graph, canvas_width=1500, canvas_height=1500, margin=10)
+    lv.run()
 
 def muestraSolucion(sol):
     print(sol[1][0],sol[1][1])
@@ -112,13 +114,13 @@ if __name__ == '__main__':
     sol = laberintoMensaje(fichEntrada)
 
     graph = sol[0]
-    es_valido=sol[4]
+    entra_en_todas=sol[4]
     es_conexo = componentes_conexos(graph)
 
-    if(es_conexo and es_valido):
+    if(es_conexo and entra_en_todas):
         muestraSolucion(sol)
-        # if len(argv) == 3 and argv[2] == "-g":
-        #     visualizaLaberinto(graph)
+        if len(argv) == 3 and argv[2] == "-g":
+            visualizaLaberinto(graph)
     else:
         print("NO ES POSIBLE CONSTRUIR EL LABERINTO")
 
