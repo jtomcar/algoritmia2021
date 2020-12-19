ac543305cc1c71d9d9e328f9fce58106e19075ed
Jonatan Tomás Caro <al375877@uji.es>
2020-12-19 10:17:26 +0100

    Funcionan algunas

--- Entregable4/entregable4_v2.py
+++ Entregable4/entregable4_v2.py
@@ -2,6 +2,7 @@ import sys
 from time import time
 from typing import *
 
+from algoritmia.problems.orderstatistics.minmax import MinMaxFinder
 from algoritmia.schemes.divideandconquer import *
 
 class Funambulista(IDivideAndConquerProblem):
@@ -38,7 +39,8 @@ class Funambulista(IDivideAndConquerProblem):
             c[k] = b[j]
             j += 1
             k += 1
-        return c
+        sol = MinMaxFinder.min_max(self, edificios)
+        return sol
 
 
     # def divide(self)
@@ -68,56 +70,88 @@ if __name__ == '__main__':
     entrada=[int(i) for i in leerFichero()]
     num_edificios = entrada[0]
     edificios = entrada[1:]
-    fun_problem = Funambulista(edificios)
-    solucion = DivideAndConquerSolver().solve(fun_problem)
+    # fun_problem = Funambulista(edificios)
+    # sol = DivideAndConquerSolver().solve(fun_problem)
 
     #************  PRUEBA **********************************************************
     sigue=True
-    solucion=list(solucion)
-    solucion.sort(reverse=True)
-    k=0
-    l=1
-    maximo=0
-    pos=0
-    pos2=0
-    for i in range(len(solucion[1:-1])):
-        for j in range(len(solucion[1:-1])):
-            if edificios[i]-edificios[j]>maximo:
-                maximo=edificios[i]-edificios[j]
-                pos=i
-                pos2=j
-    print(pos, pos2, maximo)
+    copia=edificios[:]
     while sigue:
-        for i in range(len(edificios)):
-            if solucion[k]==edificios[i]:
-                x=i
-            if solucion[l]==edificios[i]:
-                y=i
-        print(min(edificios))
-        # #if edificios[min(x,y)]>edificios[max(x,y)]:
-        # if min(edificios) not in edificios[min(x,y):max(x,y)]:
-        #     k=k+1
-        #     l=l+1
-        #     print("1")
-        # else:
-        sigue = False
-        for elem in edificios[min(x,y)+1:max(x,y)]:
-            if elem>=min(edificios[x],edificios[y]):
-                print("2")
-                k = k + 1
-                l = l + 1
-                sigue=True
+        print("tamCopia",len(copia))
+        print("tamEdif", len(edificios))
+        fun_problem = Funambulista(copia)
+        sol = DivideAndConquerSolver().solve(fun_problem)
+        sigue=True
+        ordenados=list(edificios)
+        ordenados.sort(reverse=True)
+        k=0
+        l=1
+        print("masAlto", sol[1])
+        masAlto=sol[1]
+        masBajo=sol[0]
+        posMasBajo=0
+        posMasAlto=0
+        segMasAlto = 0
+        posSegMasAlto = 0
+        for i in range(len(ordenados)):
+            if ordenados[i]==masAlto:
+                segMasAlto=ordenados[i+1]
                 break
 
-        print(edificios[min(x,y)], edificios[max(x,y)])
+        for i in range(len(edificios)):
+            if edificios[i]==masBajo:
+                posMasBajo=i
+            if edificios[i]==masAlto:
+                posMasAlto=i
+            if edificios[i]==segMasAlto:
+                posSegMasAlto=i
+                break
 
-    solucion2=edificios[min(x,y):max(x,y)]
-    solucion2.sort()
-    for i in range(len(edificios)):
-        if solucion2[0]==edificios[i]:
-            z=i
+        print(posMasBajo,posMasAlto)
+        print("segMasAlto", segMasAlto, posSegMasAlto)
 
-    print(min(x,y),max(x,y),z,min(edificios[x],edificios[y])-edificios[z])
+        if masBajo in edificios[min(posMasAlto,posSegMasAlto):max(posMasAlto,posSegMasAlto)]:
+            print("Esta entre ellos")
+            print(posSegMasAlto, posMasAlto, posMasBajo)
+            sigue=False
+        else:
+            copia.remove(masAlto)
+
+
+
+
+
+    # while sigue:
+    #     for i in range(len(edificios)):
+    #         if ordenados[k]==edificios[i]:
+    #             x=i
+    #         if ordenados[l]==edificios[i]:
+    #             y=i
+    #     print(min(edificios))
+    #     # #if edificios[min(x,y)]>edificios[max(x,y)]:
+    #     # if min(edificios) not in edificios[min(x,y):max(x,y)]:
+    #     #     k=k+1
+    #     #     l=l+1
+    #     #     print("1")
+    #     # else:
+    #     sigue = False
+    #     for elem in edificios[min(x,y)+1:max(x,y)]:
+    #         if elem>=min(edificios[x],edificios[y]):
+    #             print("2")
+    #             k = k + 1
+    #             l = l + 1
+    #             sigue=True
+    #             break
+    #
+    #     print(edificios[min(x,y)], edificios[max(x,y)])
+    #
+    # solucion2=edificios[min(x,y):max(x,y)]
+    # solucion2.sort()
+    # for i in range(len(edificios)):
+    #     if solucion2[0]==edificios[i]:
+    #         z=i
+    #
+    # print(min(x,y),max(x,y),z,min(edificios[x],edificios[y])-edificios[z])
 
     #******************************************************************************
 
