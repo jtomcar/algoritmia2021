f15ebcb92a231b439e1f4f4a958ceb2b1f38163a
Jonatan Tomás Caro <al375877@uji.es>
2020-12-20 13:23:33 +0100

    Casi_Acabado

--- Entregable4/entregable4.py
+++ Entregable4/entregable4.py
@@ -4,58 +4,57 @@ from typing import *
 
 from algoritmia.schemes.divideandconquer import *
 
-def trocea(edificios, k, i):
-    edif2 = valle = altura = posValle = posEdif1 = posEdif2 = 0
-    minimo = max(edificios)
-    while True:
-        edif1 = edificios[k]
-        if edificios[i] > edif1 and valle == 0:
-            k = i
-        if edificios[i] < edif1 and edificios[i] < minimo:
-            valle = edificios[i]
-            minimo = edificios[i]
-            posValle = i
-        if edificios[i] >= edif1 and valle != 0:
-            edif2 = edificios[i]
-            posEdif2 = i
-            posEdif1 = k
-            altura = edif1 - valle
-            break
-        if i == len(edificios) - 1:
-            break
-        i = i + 1
-    return (posEdif1,posEdif2,posValle,altura,posEdif2,posEdif2)
-
 class Funambulista(IDivideAndConquerProblem):
-    def __init__(self, edificios: List[int], sol: List[int], k, i):
+    def __init__(self, edificios: List[int], sol: List[int], e, b):
         self.edificios = edificios
-        self.k = k
-        self.i = i
+        self.b = b
+        self.e = e
         self.sol = sol
+        self.posMax=self.edificios.index(max(self.edificios))
 
     def is_simple(self) -> "bool":
-        return len(self.sol) >= self.k+1
+        return len(self.edificios) <= 1
 
     def trivial_solution(self):
-        if len(edificios) <=2:
-            return 0
-        return self.sol
+        return self.edificios
 
     def divide(self) -> Iterable["Funambulista"]:
-        trozo=trocea(self.edificios,self.k,self.i)
-        self.sol.append(trozo[:4])
-        yield Funambulista(self.edificios, self.sol, trozo[4], trozo[5])
+        yield Funambulista(self.edificios[:self.posMax], self.sol, self.e, self.posMax)
+        yield Funambulista(self.edificios[self.posMax:], self.sol, self.e, self.posMax)
 
-    def combine(self, s: Iterable[List[int]]) -> Union[tuple, int]:
-        a = tuple(s)
-        maximo = 0
-        solucion=list()
-        for elem in self.sol:
-            if elem[3] > maximo:
-                maximo = elem[3]
-                solucion = elem
 
-        return solucion
+    def combine(self, s: Iterable[List[int]]) -> List[int]:
+        if self.posMax != 0:
+            a, b = tuple(s)
+            valle = altura = posValle = posEdif1 = posEdif2 = 0
+            minimo = max(edificios)
+            a = b = self.b
+            while True:
+                edif1 = edificios[b]
+                if edificios[a] > edif1 and valle == 0:
+                    b = a
+                if edificios[a] < edif1 and edificios[a] < minimo:
+                    valle = edificios[a]
+                    minimo = edificios[a]
+                    posValle = a
+                if edificios[a] >= edif1 and valle != 0:
+                    posEdif2 = a
+                    posEdif1 = b
+                    altura = edif1 - valle
+                    break
+                if a == len(edificios) - 1:
+                    break
+                a = a + 1
+            self.sol.append((posEdif1,posEdif2,posValle,altura))
+            maximo = 0
+            solucion=list()
+            for elem in self.sol:
+                if elem[3] > maximo:
+                    maximo = elem[3]
+                    solucion = elem
+            return solucion
+        self.edificios=list()
+        return self.sol
 
 # ******************************************************************************************************
 #    METODO DE ENTRADA
@@ -73,19 +72,23 @@ def leerFichero():
 if __name__ == '__main__':
     tiempo_inicial = time()
 
-    entrada = [int(i) for i in leerFichero()]
+    entrada = [int(a) for a in leerFichero()]
     num_edificios = entrada[0]
     edificios = entrada[1:]
 
     edificios2=list()
-    for i in reversed(edificios):
-        edificios2.append(i)
+    for a in reversed(edificios):
+        edificios2.append(a)
     reves=edificios2[:]
 
-    fun_problem = Funambulista(edificios, list(), 0, 0)
-    fun_problemR = Funambulista(reves, list(), 0, 0)
+    fun_problem = Funambulista(edificios, list(), edificios, 0)
+    fun_problemR = Funambulista(reves, list(), reves, 0)
     solucion = list(DivideAndConquerSolver().solve(fun_problem))
     solucionR = list(DivideAndConquerSolver().solve(fun_problemR))
+
+    # print("SOLUCION")
+    # print(solucion)
+    # print(solucionR)
 
     if not solucion and not solucionR:
         print("NO HAY SOLUCIÓN")
