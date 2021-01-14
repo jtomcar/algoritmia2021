87b3c9e319a863206d441bda7b84cc25c5786593
rhackon <al375877@uji.es>
2021-01-14 11:22:32 +0100

    No funciona

--- Entregable5/entregable5.py
+++ Entregable5/entregable5.py
@@ -1,11 +1,13 @@
 import sys
+from time import time
 
-
-def mayor_beneficio_mem_solve(m, n, M, N, v):
+def mayor_beneficio_mem_solve(m, n, M, N, v, d):
     def L(m, n):
         if m >= M or n >= N: return 0
+        if m == M-1 and n == N-1: return v[m, n]
         if (m, n) not in mem:
-            mem[m, n] = max(L(m, n + 1), L(m + 1, n)) + v[m, n]
+            if (m,n) in d:
+                mem[m, n] = max(L(m, n + 1), L(m + 1, n)) + v[m, n]
         return mem[m, n]
 
     mem = dict()
@@ -21,25 +23,27 @@ def leerFichero():
     v=dict()
     M, N = sys.stdin.readline().split(" ")
     x = sys.stdin.readline()
+    diamantes=set()
     for m in range(int(M)):
         for n in range(int(N)):
             v[m, n] = 0
     for elem in sys.stdin.readlines():
         a, b, c = elem.split(" ")
         v[int(a), int(b)] = int(c)
+        diamantes.add((a,b))
 
-    return M, N, v
+    return M, N, v, diamantes
 
 # ******************************************************************************************************
 #    MAIN
 # ******************************************************************************************************
 
 if __name__ == '__main__':
-    # tiempo_inicial = time()
+    tiempo_inicial = time()
 
     sys.setrecursionlimit(5000) #Para que no limite la pila por las limitaciones de python
 
-    M,N,v = leerFichero()
+    M,N,v, d = leerFichero()
 
     # nf, nc = entrada[0].split(" ")
     # diamantes=entrada[2:]
@@ -52,9 +56,9 @@ if __name__ == '__main__':
     #     a, b, c = entrada[i + 2].split(" ")
     #     v[int(a), int(b)] = int(c)
 
-    print(mayor_beneficio_mem_solve(0, 0, int(M), int(N), v))
+    print(mayor_beneficio_mem_solve(0, 0, int(M), int(N), v, d))
     #
-    # tiempo_final = time()
-    # tiempo_ejecucion = tiempo_final - tiempo_inicial
-    # print("")
-    # print("El tiempo de ejecucion fue:", tiempo_ejecucion, "segundos")  # En segundos
+    tiempo_final = time()
+    tiempo_ejecucion = tiempo_final - tiempo_inicial
+    print("")
+    print("El tiempo de ejecucion fue:", tiempo_ejecucion, "segundos")  # En segundos
