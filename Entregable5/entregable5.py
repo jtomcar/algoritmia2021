db7081b3f8d5df5fc71fc42b735d1670f29bd093
rhackon <al375877@uji.es>
2021-01-15 18:52:22 +0100

    No funciona

--- Entregable5/entregable5.py
+++ Entregable5/entregable5.py
@@ -1,19 +1,15 @@
 import sys
 from time import time
 
-def mayor_beneficio_mem_solve(m, n, M, N, v, d):
+def mayor_beneficio_mem_solve(m, n, M, N, v, x):
     def L(m, n):
         if m >= M or n >= N: return 0
-        if m == M-1 and n == N-1: return v[m, n]
         if (m, n) not in mem:
-            if (m,n) in d:
-                mem[m, n] = max(L(m, n + 1), L(m + 1, n)) + v[m, n]
+            mem[m, n] = max(L(m, n + 1), L(m + 1, n)) + v[m, n]
         return mem[m, n]
 
-    mem = dict()
-    return L(m, n)
-
-
+    mem = {}
+    return L(m,n)
 
 # ******************************************************************************************************
 #    METODO DE ENTRADA
@@ -23,16 +19,14 @@ def leerFichero():
     v=dict()
     M, N = sys.stdin.readline().split(" ")
     x = sys.stdin.readline()
-    diamantes=set()
     for m in range(int(M)):
         for n in range(int(N)):
             v[m, n] = 0
     for elem in sys.stdin.readlines():
         a, b, c = elem.split(" ")
         v[int(a), int(b)] = int(c)
-        diamantes.add((a,b))
 
-    return M, N, v, diamantes
+    return M, N, v, x
 
 # ******************************************************************************************************
 #    MAIN
@@ -43,7 +37,7 @@ if __name__ == '__main__':
 
     sys.setrecursionlimit(5000) #Para que no limite la pila por las limitaciones de python
 
-    M,N,v, d = leerFichero()
+    M,N,v,x= leerFichero()
 
     # nf, nc = entrada[0].split(" ")
     # diamantes=entrada[2:]
@@ -56,7 +50,7 @@ if __name__ == '__main__':
     #     a, b, c = entrada[i + 2].split(" ")
     #     v[int(a), int(b)] = int(c)
 
-    print(mayor_beneficio_mem_solve(0, 0, int(M), int(N), v, d))
+    print(mayor_beneficio_mem_solve(0, 0, int(M), int(N), v, x))
     #
     tiempo_final = time()
     tiempo_ejecucion = tiempo_final - tiempo_inicial
