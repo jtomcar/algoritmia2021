57d26e878e98cc27955788e0d7d11891c2195454
Jonatan Tomás Caro <al375877@uji.es>
2021-01-16 17:30:20 +0100

    Final

--- Entregable5/entregable5.py
+++ Entregable5/entregable5.py
@@ -1,15 +1,18 @@
 import sys
 from time import time
 
-def mayor_beneficio_mem_solve(m, n, M, N, v, x):
+def mayor_beneficio_mem_solve(m, n, M, N, v, d):
     def L(m, n):
         if m >= M or n >= N: return 0
         if (m, n) not in mem:
-            mem[m, n] = max(L(m, n + 1), L(m + 1, n)) + v[m, n]
+            maximo=max(L(m+1,n), L(m,n+1)) + v[m, n]
+            mem[m, n] = maximo
         return mem[m, n]
 
     mem = {}
-    return L(m,n)
+    return L(m, n)
+
+
 
 # ******************************************************************************************************
 #    METODO DE ENTRADA
@@ -19,14 +22,16 @@ def leerFichero():
     v=dict()
     M, N = sys.stdin.readline().split(" ")
     x = sys.stdin.readline()
+    diamantes=set()
     for m in range(int(M)):
         for n in range(int(N)):
             v[m, n] = 0
     for elem in sys.stdin.readlines():
         a, b, c = elem.split(" ")
         v[int(a), int(b)] = int(c)
+        diamantes.add((a,b))
 
-    return M, N, v, x
+    return M, N, v, diamantes
 
 # ******************************************************************************************************
 #    MAIN
@@ -37,20 +42,9 @@ if __name__ == '__main__':
 
     sys.setrecursionlimit(5000) #Para que no limite la pila por las limitaciones de python
 
-    M,N,v,x= leerFichero()
+    M,N,v, d = leerFichero()
 
-    # nf, nc = entrada[0].split(" ")
-    # diamantes=entrada[2:]
-    # print(diamantes)
-    # v = dict()
-    # for m in range(int(nf)):
-    #     for n in range(int(nc)):
-    #         v[m, n] = 0
-    # for i in range(len(entrada[2:])):
-    #     a, b, c = entrada[i + 2].split(" ")
-    #     v[int(a), int(b)] = int(c)
-
-    print(mayor_beneficio_mem_solve(0, 0, int(M), int(N), v, x))
+    print(mayor_beneficio_mem_solve(0, 0, int(M), int(N), v, d ))
     #
     tiempo_final = time()
     tiempo_ejecucion = tiempo_final - tiempo_inicial
