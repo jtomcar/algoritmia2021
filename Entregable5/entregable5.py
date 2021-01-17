9f076649717f3c5264c0a097dfd215bbfbc680df
Jonatan Tomás Caro <al375877@uji.es>
2021-01-17 20:38:26 +0100

    Final2

--- Entregable5/entregable5.py
+++ Entregable5/entregable5.py
@@ -1,21 +1,21 @@
 import sys
-# from time import time
+from time import time
 
-def mayor_beneficio_mem_solve(m, n, M, N, v, d):
+def mayor_beneficio_mem_solve( M, N, v ):
     def L(m, n):
-        if m >= M or n >= N: return 0
+        if m < 0 or n < 0: return 0
+        if m == 0 and n == 0: return 0
         if (m, n) not in mem:
-            if m == M-1:
-                mem[m, n] = L(m,n+1) + v[m,n]
-            if n == N-1:
-                mem[m,n] = L(m+1,n) + v[m,n]
+            if n == 0:
+                mem[m, n] = L(m, n - 1) + v[m, n]
+            if m == 0:
+                mem[m, n] = L(m - 1, n) + v[m, n]
             else:
-                mem[m, n] = max(L(m+1,n), L(m,n+1)) + v[m, n]
+                mem[m, n] = max(L(m - 1, n), L(m, n - 1)) + v[m, n]
         return mem[m, n]
 
     mem = {}
-    return L(m, n)
-
+    return L(M, N)
 
 
 # ******************************************************************************************************
@@ -23,34 +23,33 @@ def mayor_beneficio_mem_solve(m, n, M, N, v, d):
 # ******************************************************************************************************
 
 def leerFichero():
-    v=dict()
+    v = dict()
     M, N = sys.stdin.readline().split(" ")
     x = sys.stdin.readline()
-    diamantes=set()
     for m in range(int(M)):
         for n in range(int(N)):
             v[m, n] = 0
     for elem in sys.stdin.readlines():
         a, b, c = elem.split(" ")
         v[int(a), int(b)] = int(c)
-        diamantes.add((a,b))
 
-    return M, N, v, diamantes
+    return M, N, v
+
 
 # ******************************************************************************************************
 #    MAIN
 # ******************************************************************************************************
 
 if __name__ == '__main__':
-    # tiempo_inicial = time()
+    tiempo_inicial = time()
 
-    sys.setrecursionlimit(5000) #Para que no limite la pila por las limitaciones de python
+    sys.setrecursionlimit(5000)  # Para que no limite la pila por las limitaciones de python
 
-    M,N,v, d = leerFichero()
+    M, N, v = leerFichero()
 
-    print(mayor_beneficio_mem_solve(0, 0, int(M), int(N), v, d ))
+    print(mayor_beneficio_mem_solve(int(M) - 1, int(N) - 1,v))
     #
-    # tiempo_final = time()
-    # tiempo_ejecucion = tiempo_final - tiempo_inicial
-    # print("")
-    # print("El tiempo de ejecucion fue:", tiempo_ejecucion, "segundos")  # En segundos
+    tiempo_final = time()
+    tiempo_ejecucion = tiempo_final - tiempo_inicial
+    print("")
+    print("El tiempo de ejecucion fue:", tiempo_ejecucion, "segundos")  # En segundos
