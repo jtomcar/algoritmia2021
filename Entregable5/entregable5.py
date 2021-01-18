69f063428bebb93cf15294dc301725f39f1aaeef
Jonatan Tomás Caro <al375877@uji.es>
2021-01-18 09:21:43 +0100

    Final3

--- Entregable5/entregable5.py
+++ Entregable5/entregable5.py
@@ -1,12 +1,7 @@
 import sys
-from time import time
-
-from algoritmia.utils import infinity
-
 
 def mayor_beneficio_mem_solve(M, N, v):
     def L(m, n):
-        if m < 0 or n < 0: return -infinity
         if m == 0 and n == 0: return 0
         if (m, n) not in mem:
             if m == 0:
@@ -17,41 +12,27 @@ def mayor_beneficio_mem_solve(M, N, v):
                 mem[m, n] = max(L(m - 1, n), L(m, n - 1)) + v[m, n]
         return mem[m, n]
     mem = {}
-    return L(M,N)
-
-
-# ******************************************************************************************************
-#    METODO DE ENTRADA
-# ******************************************************************************************************
-
-def leerFichero():
-    v = dict()
-    M, N = sys.stdin.readline().split(" ")
-    x = sys.stdin.readline()
-    for m in range(int(M)):
-        for n in range(int(N)):
-            v[m, n] = 0
-    for elem in sys.stdin.readlines():
-        a, b, c = elem.split(" ")
-        v[int(a), int(b)] = int(c)
-
-    return M, N, v
-
+    return L(M, N)
 
 # ******************************************************************************************************
 #    MAIN
 # ******************************************************************************************************
 
 if __name__ == '__main__':
-    tiempo_inicial = time()
+    sys.setrecursionlimit(5000)
+    v = {}
+    M, N = sys.stdin.readline().split(" ")
+    M = int(M)
+    N = int(N)
+    x = sys.stdin.readline()
+    for elem in sys.stdin.readlines():
+        a, b, c = elem.split(" ")
+        v[int(a), int(b)] = int(c)
+    for m in range(M):
+        for n in range(N):
+            if len(v)==M*N:
+                break
+            if (m,n) not in v:
+                v[m, n] = 0
 
-    sys.setrecursionlimit(5000)  # Para que no limite la pila por las limitaciones de python
-
-    M, N, v = leerFichero()
-
-    print(mayor_beneficio_mem_solve(int(M) - 1, int(N) - 1, v))
-    #
-    tiempo_final = time()
-    tiempo_ejecucion = tiempo_final - tiempo_inicial
-    print("")
-    print("El tiempo de ejecucion fue:", tiempo_ejecucion, "segundos")  # En segundos
+    print(mayor_beneficio_mem_solve(M - 1, N - 1, v))
